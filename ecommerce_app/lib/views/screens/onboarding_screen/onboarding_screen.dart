ddfcd43c63c203c03ece625977851965deb1de03
Ashikur Rahman Nihal <arnihal.cs@gmail.com>
2026-01-20 21:55:09 +0600

    Update onboarding_screen.dart

--- ecommerce_app/lib/views/screens/onboarding_screen/onboarding_screen.dart
+++ ecommerce_app/lib/views/screens/onboarding_screen/onboarding_screen.dart
@@ -1,28 +1,17 @@
 import 'package:flutter/material.dart';
 
-class OnboardingScreen extends StatefulWidget {
+class OnboardingScreen extends StatelessWidget {
   const OnboardingScreen({super.key});
 
-  @override
-  State<OnboardingScreen> createState() => _OnboardingScreenState();
-}
-
-class _OnboardingScreenState extends State<OnboardingScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Center(
-              child: Image.asset(
-                "assets/images/onboarding_screen_1.png",
-                height: 250,
-              ),
-            ),
-            Text("hdfajkhgbdsjkhfbs",style: TextStyle(fontSize: 28),),
+            Image.asset("assets/images/onboarding_screen_1.png", height: 250),
+            Spacer(),
+            Text("fgfd fdgdf gfg dfsd  fg fgdfsgds dfgsd"),
           ],
         ),
       ),
